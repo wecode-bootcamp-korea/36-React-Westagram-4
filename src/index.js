ddbd805c152aefa475db33157383b54f75ab5e83
Pulan <ekgus5095@gmail.com>
2022-08-03 17:30:09 +0900

    Modify: Folder

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Route } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Route />);
