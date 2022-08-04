be0fd0630b229e3fca207ce1f7cc2f765dccd011
MoonHayden <hdmoon127@gmail.com>
2022-08-04 16:49:02 +0900

    Nav-Bar

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Nav.scss';
+
+const Nav = () => {
+  return (
+    <nav className="top">
+      <div className="instatopleft">
+        <div className="mainBorder">
+          <img
+            className="instaTopImgInsta"
+            src="images/instagram.png"
+            alt="인스타"
+          />
+        </div>
+        <div className="maintitle">
+          <h1>westagram</h1>
+        </div>
+      </div>
+      <div className="barBorder">
+        <img className="search" src="images/search.png" alt="서치" />
+        <input type="text" id="searchbar" placeholder="검색" />
+      </div>
+      <div className="imgRightTop">
+        <img
+          className="instaTopImgExplore"
+          src="images/explore.png"
+          alt="탐색"
+        />
+        <img className="instaTopImgHeart" src="images/love.png" alt="하트" />
+        <img
+          className="instaTopImgMypage"
+          src="images/mapage.png"
+          alt="마이페이지"
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
