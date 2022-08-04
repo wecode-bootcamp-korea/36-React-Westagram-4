73f887f66f576a7e76b29a21f6290161fa24aec2
Pulan <ekgus5095@gmail.com>
2022-08-04 10:10:13 +0900

    really setting completed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import Router from './Router';
 
 import './styles/common.scss';
 import './styles/reset.scss';
-import './styles/variables.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Router />);
