8e6cd152adc450dd87ec2e189125e42970626444
MoonHayden <hdmoon127@gmail.com>
2022-08-09 23:26:54 +0900

    상수 데이터를 별도의 파일로 분리하여 동일하게 구현

--- src/pages/bosungMoon/Main/Footer.js
+++ src/pages/bosungMoon/Main/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FOOTER_INFO } from './uiData';
 
 const FooterInfo = () => {
   return (
@@ -12,12 +13,3 @@ const FooterInfo = () => {
   );
 };
 export default FooterInfo;
-
-const FOOTER_INFO = [
-  { id: 1, footerName: 'instagram 정보∙지원∙홍보∙센터∙API∙채용' },
-  {
-    id: 2,
-    footerName: '정보∙개인정보처리방침∙약관∙디렉터리∙프로필∙해시테그∙언어',
-  },
-  { id: 3, footerName: 'ⓒ 2019 INSTAGRAM' },
-];
