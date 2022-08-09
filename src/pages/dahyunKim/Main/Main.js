ef31d7221ec7cf931af83a81b51e43b47756e999
Pulan <ekgus5095@gmail.com>
2022-08-09 21:42:33 +0900

    Modify : Changing fetch to axios

--- src/pages/dahyunKim/Main/Main.js
+++ src/pages/dahyunKim/Main/Main.js
@@ -1,27 +1,19 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 import Nav from './Components/Nav/Nav';
 import MainContainer from './Components/MainContainer/MainContainer';
 
 import './Main.scss';
-import { useState, useEffect } from 'react';
 
 const MainDahyun = () => {
   const [postData, setPostData] = useState([]);
 
   useEffect(() => {
-    const checkStatus = res => {
-      //* 주소로 데이터를 받아오다가 에러가 생기면 res.ok = false가 되는데 파일로 받아오면 계속 true만 나와서 실행이 안된다...
-      if (!res.ok) throw new Error(`Again Check Status: ${res.status}`);
-      return res.json();
-    };
-    const uploadPostData = data => setPostData(data);
-    const getPosts = url => {
-      return fetch(url);
-    };
+    const uploadPostData = ({ data }) => setPostData(data);
+    const getPosts = url => axios.get(url);
 
-    getPosts('/data/post.json')
-      .then(checkStatus)
-      .then(uploadPostData)
-      .catch(error => console.error(error));
+    getPosts('/data/post.json').then(uploadPostData).catch(console.error);
   }, []);
 
   return (
