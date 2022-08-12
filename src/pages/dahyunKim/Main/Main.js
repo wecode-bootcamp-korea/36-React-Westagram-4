33603b98ac93a7f345fd0843f94a98808fc12ebd
Pulan <ekgus5095@gmail.com>
2022-08-12 13:07:33 +0900

    Modify : 검색 기능 리펙토링

--- src/pages/dahyunKim/Main/Main.js
+++ src/pages/dahyunKim/Main/Main.js
@@ -9,11 +9,9 @@ import './Main.scss';
 const MainDahyun = () => {
   const [postData, setPostData] = useState([]);
   const [checkSearch, setCheckSearch] = useState('');
-  const [copyData, setCopyData] = useState([]);
 
   useEffect(() => {
     const checkStatus = res => {
-      //* 주소로 데이터를 받아오다가 에러가 생기면 res.ok = false가 되는데 파일로 받아오면 계속 true만 나와서 실행이 안된다...
       if (!res.ok) throw new Error(`Again Check Status: ${res.status}`);
       return res.json();
     };
@@ -28,22 +26,14 @@ const MainDahyun = () => {
       .catch(error => console.error(error));
   }, []);
 
-  useEffect(() => {
-    const copy = [...postData];
-    setCopyData(
-      copy.filter(
-        item =>
-          checkSearch.length >= 2 &&
-          (item.userName.toLowerCase() === checkSearch ||
-            item.userName.toLowerCase().includes(checkSearch))
-      )
-    );
-  }, [checkSearch, postData]);
+  const searchFilter = postData.filter(item =>
+    item.userName.toLowerCase().includes(checkSearch.toLowerCase())
+  );
 
   return (
     <div className="mainApp">
       <Nav setCheckSearch={setCheckSearch} />
-      <MainContainer postData={copyData.length > 0 ? copyData : postData} />
+      <MainContainer postData={searchFilter} />
     </div>
   );
 };
